29e556a8fdba11bea82d26978f6c872f7a93b25d
John Shearer <john@ultranaut.com>
2016-12-08 15:46:50 -0500

    Remove some old temporary Meredith winners code
    Left over from Resolute's migration

--- app/views/partials/winners.php
+++ app/views/partials/winners.php
@@ -6,26 +6,7 @@
         $i = 0;
         foreach ($winners as $key => $winner):
             $img_tag = sprintf('<img %s="%s"/>', ($i++ >= 4 && !@$disable_slideshow) ? 'data-src' : 'src', $winner['prize_img1']);
-            if (@$winner['_override']) {
-                /*
-                no links for 8 days of Meredith prizes at launch
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                THIS CONDITIONAL CAN BE REMOVED AFTER PROJECT HAS BEEN RUNNING FOR 8 DAYS
-                */
-                $a_tag = sprintf('<a>%s</a>', $winner['prize_title']);
-            } else {
-                $a_tag = sprintf('<a href="/prize/%s">%s</a>', $winner['date'], $winner['prize_title']);
-            }
+            $a_tag = sprintf('<a href="/prize/%s">%s</a>', $winner['date'], $winner['prize_title']);
             ?><div><?= $img_tag ?><div>
                     <h6><?= date("F j", strtotime($winner['date'])); ?></h6>
                     <h5><?= $a_tag ?></h5>
